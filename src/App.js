66b8e0fba795460404eda5240e5bc89ae96e69b6
facefall <3108999582@qq.com>
2019-10-23 09:06:29 +0800

    Signed-off-by: facefall <3108999582@qq.com>

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import Header from "./component/header";
+import Banner from "./component/banner";
+
+
+
+
+class App extends Component {
+    render() {
+        return (
+            <div className={'App'}>
+                <Header/>
+                <Banner/>
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+
+import "./common/css/common.css";
+import "./component/header/header.scss";
+import "./component/banner/banner.scss";
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
+
+
