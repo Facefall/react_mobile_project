a389bd7734c57652bd1b4a262eecdfcf542c324b
facefall <3108999582@qq.com>
2019-10-23 12:50:54 +0800

    rebuild environment

---
+++ src/component/banner/index.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+// import ""
+// import  from "/node_modules/_swiper@5.1.0@swiper/";
+// import
+//swiper
+
+class Banner extends Component {
+    render() {
+        return (
+            <div className={'banner'}>
+                2
+            </div>
+        );
+    }
+}
+
+export default Banner;
---
+++ src/component/header/index.jsx
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <div className={'App-header'}>
+                    <div className={'App-logo'}>
+                        <img src={require('../../img/logo.png')} alt=""/>
+                    </div>
+                    <div className={'App-search'}>
+                        <i className={'search-icon iconfont icon-sousuo'}></i>
+                        <input type="text" defaultValue={"搜索商品名称"}/>
+                    </div>
+                    <div className={'App-login'}>
+                        <i className={'user iconfont icon-yonghu'}></i>
+                    </div>
+                </div>
+                <div className={'App-nav'}>
+                    <div className={'h-nav'}>
+                        <ul>
+                            {/*
+                                onclick p classname = 'active
+                            */}
+                            <li><p>推荐</p></li>
+                            <li><p>手机</p></li>
+                            <li><p>智能</p></li>
+                            <li><p>电视</p></li>
+                            <li><p>笔记本</p></li>
+                            <li><p>家电</p></li>
+                            <li><p><i className={'iconfont icon-xiangxia'}/></p></li>
+                            {/*<li><p>生活周边</p></li>*/}
+                        </ul>
+                    </div>
+
+                </div>
+            </header>
+        );
+    }
+}
+
+export default Header;
