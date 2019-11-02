5977d800713550ff9ec70dc95b004d2a363b2d4c
facefall <3108999582@qq.com>
2019-11-02 17:11:54 +0800

    backheader随页面跳转 更改subtitle 组件传值

--- src/component/11-shopCartPage/index.jsx
+++ src/component/11-shopCartPage/index.jsx
@@ -3,15 +3,12 @@ import Footer from "../09-footer";
 import BackHeader from "../011-backHeader";
 import CartList from "./CartList";
 
-
-
-
 class Index extends Component {
     render() {
         return (
             <div>
                 <div className="Cart">
-                    <BackHeader/>
+                    <BackHeader subtitle={'购物车'} />
                     <CartList/>
                 </div>
                 <Footer/>
