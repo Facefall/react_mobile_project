eee36c3a75d8865fa3200655d766a3201b92525f
facefall <3108999582@qq.com>
2019-11-02 17:06:44 +0800

    完善 底部导航栏点击跳转变声功能

--- src/component/09-footer/index.jsx
+++ src/component/09-footer/index.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-// import  from "";
 import {
     NavLink
 } from 'react-router-dom'
@@ -9,30 +8,51 @@ import "./footer.scss";
 
 
 class Index extends Component {
+    componentDidMount() {
+        this._footer.addEventListener("touchend",(e) => {
+            e.target.classList.toggle('active');
+            // if (e.targe)
+            // if (e.target.classList.contains('active')){
+            //     e.target.classList.remove('active')
+            // } else{
+            //     e.target.classList.add('active')
+            // }
+            // console.log(e.target);
+            // this._Cart.style='color:#ff6700';
+
+        })
+    }
+
+    //触发顺序 touchStart toucheEnd click
+    //touchEnd
+    // handleClick = (className) =>{
+    //     console.log(className);
+    // };
+
     render() {
         return (
-            <footer>
-                <NavLink to={'/'} >
+            <footer ref={footer=>this._footer = footer}>
+                <NavLink exact to={'/'} ref={div => this._home = div}>
                     <div className={'Home'} >
                         <i className={'iconfont icon-shouye'} />
                         <p>首页</p>
                     </div>
                 </NavLink>
-                <NavLink to={'/'} >
-                    <div className={'Category'} >
+                <NavLink to={'buyPage'} ref={div => this._Category = div}>
+                    <div className={'Category'}  >
                         <i className={'iconfont icon-icon-'} />
                         <p>分类</p>
                     </div>
                 </NavLink>
 
-                <NavLink to={'shopCart'} >
-                    <div className={'Cart'} >
+                <NavLink to={'shopCart'} ref={div=>this._Cart = div}>
+                    <div className={'Cart'}   >
                         <i className={'iconfont icon-gouwuche'} />
                         <p>购物车</p>
                     </div>
                 </NavLink>
-                <NavLink to={'myPage'} >
-                    <div className={'User'} >
+                <NavLink to={'myPage'} ref={div=>this._use = div}>
+                    <div className={'User'}  >
                         <i className={'iconfont icon-yonghu'} />
                         <p>我的</p>
                     </div>
