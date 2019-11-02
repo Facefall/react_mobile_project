84bdc233b043b9560b1b00d55a3c62dd98ab226c
facefall <3108999582@qq.com>
2019-11-02 15:29:11 +0800

    添加购物车以及分类header样式

--- src/component/011-backHeader/index.jsx
+++ src/component/011-backHeader/index.jsx
@@ -1,13 +1,25 @@
 import React, {Component} from 'react';
 import "./index.scss";
+import {
+    NavLink
+} from "react-router-dom";
+// import MainPage from "../12-mainPage";
+
+
 
 class Index extends Component {
+    state={
+        subtitle:'购物车'
+    };
+
     render() {
         return (
             <header  >
                 <div className={'backHeader'}>
-                    <img src={require('../../img/arrow.png')} alt="arrow" className="arrow"/>
-                    <span>分类</span>
+                    <NavLink to={'/'}>
+                        <img src={require('../../img/arrow.png')} alt="arrow" className="arrow"/>
+                    </NavLink>
+                    <span>{this.state.subtitle}</span>
                     <i className={'iconfont icon-sousuo'} />
                 </div>
             </header>
