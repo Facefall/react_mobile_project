aed7572dc2cd92f7c912eb31a940602753682610
facefall <3108999582@qq.com>
2019-11-02 17:05:26 +0800

    完成notfound 404页面

--- src/component/notfound404/index.jsx
+++ src/component/notfound404/index.jsx
@@ -5,7 +5,14 @@ class Index extends Component {
     render() {
         return (
             <div className={'notfound'}>
+
                 <img src={require('./img/404.jpg')} alt=""/>
+                <p className="sorry">
+                    咦~页面不见了~
+                </p>
+                <p className="back">
+                    <a href="http://mi.com">返回首页 mi.com</a>
+                </p>
             </div>
         );
     }
